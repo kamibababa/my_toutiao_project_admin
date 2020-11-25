026f53ad3e2bb3d1f2661dd46dc50832182d9658
zhangsan <zhangsan@sohu.com>
2020-11-25 11:25:26 +0800

    add_axios_intercepor

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,11 +10,34 @@ import axios from 'axios'
 // 创建一个 axios 实例，说白了就是复制了一个 axios
 // 我们通过这个实例去发请求，把需要的配置配置给这个实例来处理
 const request = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
-  // baseURL: 'http://127.0.0.1:5000/'
+  // baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
+  baseURL: 'http://127.0.0.1:5000/'
 })
 
 // 请求拦截器
+request.interceptors.request.use(
+  // 任何所有请求会经过这里
+  // config 是当前请求相关的配置信息对象
+  // config 是可以修改的
+  function (config) {
+    const user = JSON.parse(window.localStorage.getItem('user'))
+
+    // 如果有登录用户信息，则统一设置 token
+    if (user) {
+      config.headers.Authorization = `Bearer ${user.token}`
+    }
+
+    // 然后我们就可以在允许请求出去之前定制统一业务功能处理
+    // 例如：统一的设置 token
+
+    // 当这里 return config 之后请求在会真正的发出去
+    return config
+  },
+  // 请求失败，会经过这里
+  function (error) {
+    return Promise.reject(error)
+  }
+)
 
 // 响应拦截器
 
