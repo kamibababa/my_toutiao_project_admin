f833cd805e73c3b051e3272d4c074e25eaa4917f
zhangsan <zhangsan@sohu.com>
2020-11-26 09:22:14 +0800

    add image2

---
+++ src/api/image.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+
+/**
+ * 获取素材列表
+ */
+export const getImages = params => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/user/images',
+    params
+  })
+}
